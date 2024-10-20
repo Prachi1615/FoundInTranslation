2c2965ba440c92554d6f0d9e81e9d2f22adbf101
Yaki <shibatales@gmail.com>
2024-10-20 01:12:00 -0700

    chore: bits

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@
 
 import SelectLanguage from "@/components/select-language";
 import TranslationView from "@/components/translation-view";
-import { useIsLoggedIn } from "@dynamic-labs/sdk-react-core";
+import { DynamicWidget, useIsLoggedIn } from "@dynamic-labs/sdk-react-core";
 import { useLanguage } from "./lib/providers/language";
 
 
@@ -11,6 +11,7 @@ const App = () => {
   const { targetLanguage, destinationLanguage } = useLanguage();
 
   return <div className="pt-4">
+    <DynamicWidget />
     <h2 className="text-2xl font-bold mb-4">{isLoggedIn ? "Choose your languages to begin translation" : "Please login to use Found In Translation"}</h2>
     {isLoggedIn && <SelectLanguage />}
     {isLoggedIn && !!targetLanguage && !!destinationLanguage && <TranslationView />}
